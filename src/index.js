06f71b575505d38639a50613e98f2830b45fead7
ky-m0 <114282955+ky-m0@users.noreply.github.com>
2025-04-19 00:19:45 +1000

    added js on form

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-//alert("Hi");
+function generatePoem(event) {
+  event.preventDefaut();
+
+  new Typewriter("#poem", {
+    strings: ["Hello world abcd "],
+    autoStart: true,
+    cursor: "",
+  });
+}
+
+let formElement = document.querySelector("#form");
+formElement.addEventListener("submit", generatePoem);
