1354dc934789851c8dc5b3ccebb06ab253eb59e5
ky-m0 <114282955+ky-m0@users.noreply.github.com>
2025-04-19 20:56:55 +1000

    updated AI prompt

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ function displayPoem(response) {
     strings: response.data.answer,
     autoStart: true,
     cursor: "",
+    speed: 10,
   });
 }
 
@@ -13,7 +14,7 @@ function generatePoem(event) {
   let instructionsInput = document.querySelector("#instructions");
   let apiKey = "0b9403b26eb5tfao06ad1ab04e3ae066";
   let context = "You are an AI poetry assistant";
-  let prompt = `Write a short nature-themed poem about ${instructionsInput.value}. Make sure that it is a haiki poem and it follows the user instructions. Please only include the poem, no additional text or code visible. Use HTMl formatting only, use a single line for each line of the poem`;
+  let prompt = `Write a short nature-themed poem about ${instructionsInput.value}. Make sure that it is a haiki poem and it follows the user instructions. Please only include the poem, no additional text or code visible, except for a title. Use HTMl formatting and saparate each line with a <br />, use a single line for each line of the poem`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   console.log("generating poem");
